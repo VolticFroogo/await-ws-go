ac9e1fbf00e833f351b9c5e1c9706f32d5a1be2e
Harry <harry@froogo.co.uk>
2020-07-14 18:09:56 +0100

    Added more Client.HandleResponse unit tests

--- handler_test.go
+++ handler_test.go
@@ -7,35 +7,90 @@ import (
 )
 
 func TestClient_HandleResponse(t *testing.T) {
-	client := NewClient(nil)
+	t.Run("Valid", func(t *testing.T) {
+		client := NewClient(nil)
 
-	wait := make(chan AwaitedResponse)
-	client.waitingResponses[0] = wait
+		wait := make(chan AwaitedResponse)
+		client.waitingResponses[0] = wait
 
-	msg := map[string]interface{}{
-		"id":       float64(0),
-		"response": true,
-		"message":  "msgContent",
-	}
-	go client.HandleResponse(msg)
+		msg := map[string]interface{}{
+			"id":       float64(0),
+			"response": true,
+			"message":  "msgContent",
+		}
+		go client.HandleResponse(msg)
 
-	var response AwaitedResponse
+		var response AwaitedResponse
 
-	select {
-	case response = <-wait:
-		break
+		select {
+		case response = <-wait:
+			break
 
-	case <-time.After(testTimeout):
-		t.Error("Timeout")
-		return
-	}
+		case <-time.After(testTimeout):
+			t.Error("Timeout")
+			return
+		}
 
-	assert.Equal(t, msg["message"], response.Message)
-	assert.Nil(t, response.Err)
+		assert.Equal(t, msg["message"], response.Message)
+		assert.Nil(t, response.Err)
 
-	client.waitingResponseMutex.Lock()
-	defer client.waitingResponseMutex.Unlock()
+		client.waitingResponseMutex.Lock()
+		defer client.waitingResponseMutex.Unlock()
 
-	_, ok := client.waitingResponses[0]
-	assert.False(t, ok)
+		_, ok := client.waitingResponses[0]
+		assert.False(t, ok)
+	})
+
+	// NOTE: I use the line:
+	// client.waitingResponses = nil
+	// to throw an error in case it is written to when it shouldn't be.
+
+	t.Run("No response", func(t *testing.T) {
+		client := NewClient(nil)
+		client.waitingResponses = nil
+
+		msg := map[string]interface{}{}
+		assert.False(t, client.HandleResponse(msg))
+	})
+
+	t.Run("Invalid response type", func(t *testing.T) {
+		client := NewClient(nil)
+		client.waitingResponses = nil
+
+		msg := map[string]interface{}{
+			"response": "badType",
+		}
+		assert.False(t, client.HandleResponse(msg))
+	})
+
+	t.Run("Response false", func(t *testing.T) {
+		client := NewClient(nil)
+		client.waitingResponses = nil
+
+		msg := map[string]interface{}{
+			"response": false,
+		}
+		assert.False(t, client.HandleResponse(msg))
+	})
+
+	t.Run("No ID", func(t *testing.T) {
+		client := NewClient(nil)
+		client.waitingResponses = nil
+
+		msg := map[string]interface{}{
+			"response": true,
+		}
+		assert.True(t, client.HandleResponse(msg))
+	})
+
+	t.Run("Invalid ID type", func(t *testing.T) {
+		client := NewClient(nil)
+		client.waitingResponses = nil
+
+		msg := map[string]interface{}{
+			"response": true,
+			"id":       "badType",
+		}
+		assert.True(t, client.HandleResponse(msg))
+	})
 }
