d5627632112390ae4dbb4e64e07cb33112003198
Harry <harry@froogo.co.uk>
2020-07-14 15:04:19 +0100

    Added Client.Request unit test

---
+++ awaitws_test.go
@@ -0,0 +1,7 @@
+package awaitws
+
+import "time"
+
+const (
+	testTimeout = time.Second
+)
--- request_test.go
+++ request_test.go
@@ -1,9 +1,129 @@
 package awaitws
 
 import (
+	"github.com/gorilla/websocket"
 	"github.com/stretchr/testify/assert"
+	"log"
+	"net/http"
+	"net/url"
 	"testing"
+	"time"
 )
+
+func setupTestClient_Request() (srv *http.Server, msgChan chan map[string]interface{}) {
+	upgrader := websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	r := http.NewServeMux()
+
+	msgChan = make(chan map[string]interface{})
+
+	r.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		// Upgrade the connection to a WebSocket.
+		c, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Printf("Upgrading connection error: %s", err)
+			return
+		}
+
+		// Safely close the WebSocket when this function dies.
+		defer c.Close()
+
+		for {
+			var msg map[string]interface{}
+			err = c.ReadJSON(&msg)
+			if err != nil && (websocket.IsCloseError(err) || websocket.IsUnexpectedCloseError(err)) {
+				break
+			}
+
+			msgChan <- msg
+		}
+	})
+
+	srv = &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go srv.ListenAndServe()
+
+	return
+}
+
+func cleanupTestClient_Request(srv *http.Server) {
+	err := srv.Close()
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+}
+
+func TestClient_Request(t *testing.T) {
+	srv, msgChan := setupTestClient_Request()
+
+	u := url.URL{Scheme: "ws", Host: srv.Addr, Path: "/ws"}
+	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	client := NewClient(conn)
+
+	msgContent := "msgContent"
+	wait, err := client.Request(msgContent)
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	var msg map[string]interface{}
+
+	select {
+	case msg = <-msgChan:
+		break
+
+	case <-time.After(testTimeout):
+		t.Error("Timeout")
+		return
+	}
+
+	assert.Equal(t, float64(0), msg["id"])
+	assert.Equal(t, true, msg["request"])
+	assert.Equal(t, msgContent, msg["message"])
+	assert.Equal(t, wait, client.waitingResponses[0])
+
+	wait, err = client.Request(msgContent)
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	select {
+	case msg = <-msgChan:
+		break
+
+	case <-time.After(testTimeout):
+		t.Error("Timeout")
+		return
+	}
+
+	assert.Equal(t, float64(1), msg["id"])
+	assert.Equal(t, true, msg["request"])
+	assert.Equal(t, msgContent, msg["message"])
+	assert.Equal(t, wait, client.waitingResponses[1])
+
+	err = conn.Close()
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	cleanupTestClient_Request(srv)
+}
 
 func TestClient_IsRequest(t *testing.T) {
 	client := NewClient(nil)
