4d5e7399b628a5dfa2fccef8790ef051fdd7f461
Harry <harry@froogo.co.uk>
2020-07-14 17:48:31 +0100

    Added Client.HandleResponse unit test

---
+++ handler_test.go
@@ -0,0 +1,38 @@
+package awaitws
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestClient_HandleResponse(t *testing.T) {
+	client := NewClient(nil)
+
+	wait := make(chan AwaitedResponse)
+	client.waitingResponses[0] = wait
+
+	msg := map[string]interface{}{
+		"id":       float64(0),
+		"response": true,
+		"message":  "msgContent",
+	}
+	go client.HandleResponse(msg)
+
+	var response AwaitedResponse
+
+	select {
+	case response = <-wait:
+		break
+
+	case <-time.After(testTimeout):
+		t.Error("Timeout")
+		return
+	}
+
+	assert.Equal(t, msg["message"], response.Message)
+	assert.Nil(t, response.Err)
+
+	_, ok := client.waitingResponses[0]
+	assert.False(t, ok)
+}
